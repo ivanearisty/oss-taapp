36fc13dc5205d10914abfb3ecccb8d8d6d50d381
ivanearisty <iae225@stern.nyu.edu>
2025-07-30 06:14:29 -0400

    MyPy fixes prior to development effort

--- src/gmail_client_impl/src/gmail_client_impl/_impl.py
+++ src/gmail_client_impl/src/gmail_client_impl/_impl.py
@@ -44,7 +44,7 @@ from typing import ClassVar
 import mail_client_api
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import InstalledAppFlow  # type: ignore[import-untyped]
 from googleapiclient.discovery import Resource, build
 
 
