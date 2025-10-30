2bc6c576988b2a5a7917fbe0f536f67ba6feb4d7
jw7914 <jw7914@nyu.edu>
2025-10-29 22:47:03 -0400

    inital skeleton with pyproject

---
+++ src/discord_client_impl/src/discord_client_impl/discord_impl.py
@@ -0,0 +1,7 @@
+"""Discord Client Implementation.
+
+This module provides a concrete implementation of the chat client API using the Discord API.
+It handles OAuth2 authentication and provides methods to interact with Discord.
+
+"""
+
